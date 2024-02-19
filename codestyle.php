e725a8e86bb0187e8cb13d8131909b2124cb710d
Aleksander Kowalski <mrhdolek14@gmail.com>
2024-02-19 19:20:01 +0100

    fix: codestyle

--- codestyle.php
+++ codestyle.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Blumilk\Codestyle\Config;
 use Blumilk\Codestyle\Configuration\Defaults\Paths;
-use Blumilk\Codestyle\Configuration\Defaults\SprzedajemyRules;
 
 $config = new Config(
     paths: new Paths(
@@ -14,7 +13,6 @@ $config = new Config(
         "tests/unit",
         "tests/codestyle",
     ),
-    rules: new SprzedajemyRules(),
 );
 
 return $config->config();
